c1b4f2652a2f7d7788b5c2a04b7f06329b4e9035
Enrico <enrico@cora.com.br>
2021-04-18 22:26:50 -0300

    removing useless import

--- domain/src/test/kotlin/com/quickstart/person/usecase/PersonUseCaseUnitTest.kt
+++ domain/src/test/kotlin/com/quickstart/person/usecase/PersonUseCaseUnitTest.kt
@@ -2,7 +2,6 @@ package com.quickstart.person.usecase
 
 import com.quickstart.person.exceptions.PersonAlreadyExistsException
 import com.quickstart.person.factory.personFactory
-import com.quickstart.person.model.PersonEventType
 import com.quickstart.person.ports.output.PersonDataAccessPort
 import com.quickstart.person.ports.output.PersonEventProducerPort
 import io.kotest.assertions.throwables.shouldThrow
